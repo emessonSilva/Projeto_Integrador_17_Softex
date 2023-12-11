153f66c09871108120126287e5716119d3dde81e
Monique Lupe <moniquelupeandrade@gmail.com>
2023-12-11 16:12:45 -0300

    :sparkles: Create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Header } from './components/header/header'
+import { BTN } from './components/botao'
 
 export default function Home() {
   return (
     <main>
-     <h1>Git Branch teste Monique</h1>
-      
-    <span>testando...</span>
+      <h1>Git Branch teste Monique</h1>  
+      <span>testando...</span>
+      <Header/>
+      <BTN/>
 
     </main>
   )
