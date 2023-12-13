890c7a3121a1c0b755a843ad9e9d791d3ea36451
marcelo <marcelolopesvarzea@gmail.com>
2023-12-13 14:18:55 -0300

    import main styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
-     <h1>Git Branch teste Monique</h1>
+    <main className={styles.main}>
+      <h1>Git Branch teste Monique</h1>
     </main>
   )
 }
