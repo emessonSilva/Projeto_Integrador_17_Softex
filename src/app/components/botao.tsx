0844c91789968585fbe93b0d6b79ade601af0398
Emesson Silva <140443316+emessonSilva@users.noreply.github.com>
2023-12-13 16:27:05 -0300

    Update botao.tsx

--- src/app/components/botao.tsx
+++ src/app/components/botao.tsx
@@ -1,13 +1,11 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 
 interface propsButton {
-    // name?: string;
-    width?: string;
-    height?: string;
-    children?: ReactNode
+  name?: string;
+  width?: string;
+  height?: string;
 }
 
-export function BTN({ children, width, height }: propsButton) {
-    return <button style={{ width, height }}>{children}</button>;
+export function BTN({ name, width, height }: propsButton) {
+  return <button style={{ width, height }}>{name}</button>;
 }
-
