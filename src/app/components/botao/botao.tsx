4fb6d67f7648cd3ba824f8d7a382cf652dfec96c
marcelo <marcelolopesvarzea@gmail.com>
2023-12-15 00:00:55 -0300

    ➕ add type to props button

--- src/app/components/botao/botao.tsx
+++ src/app/components/botao/botao.tsx
@@ -4,8 +4,16 @@ interface propsButton {
   name?: string;
   width?: string;
   height?: string;
+  type?: "submit" | "reset" | "button" | undefined;
+
 }
 
-export function BTN({ name, width, height }: propsButton) {
-  return <button style={{ width, height }}>{name}</button>;
+export function BTN({ name, width, height, type }: propsButton) {
+  return <button
+    type={type}
+    style={{ width, height }}
+  >
+    {name}
+
+  </button>;
 }
