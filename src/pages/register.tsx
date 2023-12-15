4fc43d10b28d7027cd855d098e22c4df0a49461b
marcelo <marcelolopesvarzea@gmail.com>
2023-12-15 00:11:55 -0300

    ✂️ ignore✂️

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,7 +4,7 @@ import { BTN } from "@/app/components/botao/botao";
 export default function Register() {
   return (
     <div>
-      <BTN name="Cadastrar" width="200px" height="100px" />
+      <BTN name="Cadastrar" width="200px" height="70px" />
     </div>
   );
 }
