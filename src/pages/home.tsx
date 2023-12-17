2035f0096057c1ccd669243aa3bde9e707b0badd
marcelo <marcelolopesvarzea@gmail.com>
2023-12-16 23:51:27 -0300

    🔥 deleted files and create news

--- src/pages/home.tsx
+++
@@ -1,5 +0,0 @@
-export default function DashBoard(){
-    return (
-        <h1>Pagina do DashBoard</h1>
-    )
-}
--- src/pages/profile.tsx
+++
@@ -1,5 +0,0 @@
-export default function Profile(){
-    return (
-        <h1>Pagina de Perfil</h1>
-    )
-}
