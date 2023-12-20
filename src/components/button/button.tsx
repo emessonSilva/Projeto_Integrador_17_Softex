efadbd47ef27ece2370ea962ba6bfa680da2c9d1
Emesson <emessonbarbosa600@gmail.com>
2023-12-19 23:17:49 -0300

    Changes on login page, button component and logo

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,17 +4,20 @@ interface propsButton {
   name?: string;
   width?: string;
   height?: string;
+  backgroundColor?: string;
+  color?: string;
+  borderRadius?: number;
   type?: "submit" | "reset" | "button" | undefined;
   onClick?: () => void;
   onSubmit?: () => void;
 }
 
-export function BTN({ name, width, height, type, onClick, onSubmit }: propsButton) {
+export function BTN({ name, width, height, backgroundColor, color, borderRadius, type, onClick, onSubmit }: propsButton) {
   return <button
     onSubmit={onSubmit}
     onClick={onClick}
     type={type}
-    style={{ width, height }}
+    style={{ width, height, backgroundColor, color, borderRadius }}
   >
     {name}
 
