53ffb560a0f63e27b12dd0d19313a4b6d07cae5c
Emesson <emessonbarbosa600@gmail.com>
2023-12-29 22:59:13 -0300

    Added: components grafics; a grafic example in the main page(dashboard).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Input } from "@/components/input/input";
 import { useState } from "react";
 import Link from 'next/link';
+import BarChart from '@/components/grafics/grafic1';
 
 export default function Home() {
 
@@ -15,10 +16,10 @@ export default function Home() {
   // function changeLoginIcon() {
   //   setLoginLogout(!loginLogout)
   // }
-
+ 
   return (
-
-    <body style={{ display: 'flex' , height: '100vh'}}>
+                                    //aqui pode botar o height:'100vh'
+    <body style={{ display: 'flex' , height: ''}}> 
 
     <aside style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '12px'}}>
     <img style={{ background: 'black', width: '80px', height: '80px', borderRadius: '100%'}} /><p>nome da empresa logada</p>
@@ -27,17 +28,17 @@ export default function Home() {
     <label htmlFor="">Data ínicio</label><Input height="30px" type='date'></Input>
     <label htmlFor="">Data Fim</label><Input height="30px" type='date'></Input>
     <label htmlFor="">Cliente</label><Input height="30px"></Input>
-    <label htmlFor="">Produtos</label><Input height="30px"></Input>
-    <label htmlFor="">Serviços</label><Input height="30px"></Input>
+    <label htmlFor="">Produto</label><Input height="30px"></Input>
+    <label htmlFor="">Serviço</label><Input height="30px"></Input>
     <p title='Logout' style={{ fontSize: '2.5rem' }}><Link style={{ color: 'black' }} href="../login"><i className="bi bi-box-arrow-in-right"></i></Link></p>
     <p title='Informações sobre Kósmos Tecnologia' style={{ fontSize: '2.5rem' }}><i className="bi bi-info-circle"></i></p>
     </aside>
 
     <main style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '50px' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }}>
 
-          <div style={{ backgroundColor: 'blue', padding: '20px', width: '500px', height: '300px', color: 'white' }}>
-            <h1>Gráfico 1</h1>
+          <div style={{padding: '20px', width: '600px', height: '400px' }}>
+          <BarChart></BarChart>
           </div>
 
           <div style={{ backgroundColor: 'green', padding: '20px', width: '500px', height: '300px', color: 'white' }}>
