c51a1e08523cb0ce834b38da2e85893df8429c97
Yakin <yakinfsilva@gmail.com>
2024-01-03 19:49:56 -0300

    aside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,22 @@
 'use client'
 
-import MyAside from '@/components/aside/MyAside'; 
+import MyAside from '@/components/aside/MyAside';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Input } from "@/components/input/input";
 import Link from 'next/link';
 import BarChart from '@/components/grafics/Bar';
 import LineChart from '@/components/grafics/Line';
-import MyAside from '@/components/aside/Myaside'
 
 export default function Home() {
-
-  // const [loginLogout, setLoginLogout] = useState<Boolean>(false)
-
-  //funçao para mudar icone de login para logout
-  // function changeLoginIcon() {
-  //   setLoginLogout(!loginLogout)
-  // }
- 
   return (
-                                    //aqui pode botar o height:'100vh'
-    <body style={{ display: 'flex' , height: ''}}> 
-
-    <aside style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '12px'}}>
-    <img style={{ background: 'black', width: '80px', height: '80px', borderRadius: '100%'}} /><p>nome da empresa logada</p>
-    <p title='Perfil' style={{ fontSize: '2.5rem' }}> <Link style={{ color: 'black' }} href="../profile"><i className="bi bi-person-circle" ></i></Link></p>
-    <p style={{ fontStyle: 'strong', fontSize: '1.5rem' }}>Filtros</p>
-    <label htmlFor="">Data ínicio</label><Input height="30px" type='date'></Input>
-    <label htmlFor="">Data Fim</label><Input height="30px" type='date'></Input>
-    <label htmlFor="">Cliente</label><Input height="30px"></Input>
-    <label htmlFor="">Produto</label><Input height="30px"></Input>
-    <label htmlFor="">Serviço</label><Input height="30px"></Input>
-    <p title='Logout' style={{ fontSize: '2.5rem' }}><Link style={{ color: 'black' }} href="../login"><i className="bi bi-box-arrow-in-right"></i></Link></p>
-    <p title='Informações sobre Kósmos Tecnologia' style={{ fontSize: '2.5rem' }}><i className="bi bi-info-circle"></i></p>
-    </aside>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <MyAside />
 
       <main style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flex: 1 }}>
         <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }}>
-
-          <div style={{padding: '20px', width: '600px', height: '400px' }}>
-          <BarChart></BarChart>
+          <div style={{ padding: '20px', width: '600px', height: '400px' }}>
+            <BarChart />
           </div>
 
           <div style={{ backgroundColor: 'blue', padding: '20px', width: '600px', height: '400px', color: 'white' }}>
@@ -53,10 +30,8 @@ export default function Home() {
           <div style={{ backgroundColor: 'purple', padding: '20px', width: '600px', height: '400px', color: 'white' }}>
             <h1>Gráfico 4</h1>
           </div>
-      </div>
-    </main>
-    
-  </body>
-
-  )
+        </div>
+      </main>
+    </div>
+  );
 }
