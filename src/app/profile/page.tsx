3859af560480e25ea641df1d91fc47481cba9e8f
Monique Lupe <moniquelupeandrade@gmail.com>
2024-01-16 21:55:41 -0300

    :sparkles: Profile page text adjustments

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -36,18 +36,18 @@ export default function Profile() {
 
     return (
         <main style={{ height: "100vh", display: "flexColun", padding: "155px" }}>
-            <h1 style={{ marginBottom: "40px" }}>Pagina de Perfil</h1>
-            <p style={{ margin: "20px", width: "100%", padding: "5px", backgroundColor: "rgb(184, 185, 185)" }}>{datas.map(e => e.nomeEmpresa)}</p>
+            <h1 style={{ marginBottom: "40px" }}>Minha conta</h1>
+            <p style={{ margin: "20px", width: "100%", padding: "5px", backgroundColor: "rgb(229, 229, 229)" }}>{datas.map(e => e.nomeEmpresa)}</p>
             <div style={{ margin: "20px", display: "flex", alignItems: "center", gap: "40px" }}>
-                <p style={{ width: "50%", backgroundColor: "rgb(184, 185, 185)" }} >{datas.map(e => e.cnpj)}</p>
-                <p style={{ width: "50%", backgroundColor: "rgb(184, 185, 185)" }} >{datas.map(e => e.cep)}</p>
+                <p style={{ width: "50%", backgroundColor: "rgb(229, 229, 229)" }} >{datas.map(e => e.cnpj)}</p>
+                <p style={{ width: "50%", backgroundColor: "rgb(229, 229, 229)" }} >{datas.map(e => e.cep)}</p>
             </div>
             <div style={{ margin: "20px", display: "flex", alignItems: "center", gap: "40px" }}>
-                <p style={{ width: "50%", padding: "5px", backgroundColor: "rgb(184, 185, 185)" }} >{datas.map(e => e.nomeProprietario)}</p>
-                <p style={{ width: "50%", padding: "5px", backgroundColor: "rgb(184, 185, 185)" }} >{datas.map(e => e.telefone)}</p>
+                <p style={{ width: "50%", padding: "5px", backgroundColor: "rgb(229, 229, 229)" }} >{datas.map(e => e.nomeProprietario)}</p>
+                <p style={{ width: "50%", padding: "5px", backgroundColor: "rgb(229, 229, 229)" }} >{datas.map(e => e.telefone)}</p>
             </div>
-            <p style={{ margin: "20px", width: "100%", padding: "5px", backgroundColor: "rgb(184, 185, 185)" }} >{datas.map(e => e.email)}</p>
-            <p style={{ margin: "20px", width: "100%", padding: "5px", backgroundColor: "rgb(184, 185, 185)" }} >{datas.map(e => e.empresaSegmento)}</p>
+            <p style={{ margin: "20px", width: "100%", padding: "5px", backgroundColor: "rgb(229, 229, 229)" }} >{datas.map(e => e.email)}</p>
+            <p style={{ margin: "20px", width: "100%", padding: "5px", backgroundColor: "rgb(229, 229, 229)" }} >{datas.map(e => e.empresaSegmento)}</p>
             <Link href="/">
                 <BTN
                     margin={20}
@@ -55,7 +55,7 @@ export default function Profile() {
                     color="white"
                     backgroundColor="#0F4EF0"
                     type="button"
-                    name="Dasboard"
+                    name="Voltar"
                     width="200px"
                     height="50px"
                 ></BTN>
